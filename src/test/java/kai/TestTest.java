e1722b64d24283463a55fb566b9363bae6886335
Kai Dainichi Christensen <dainichi@gmail.com>
2018-12-31 00:25:24 +0900

    initial commit

---
+++ src/test/java/kai/TestTest.java
@@ -0,0 +1,15 @@
+package kai;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestTest {
+
+	@Test
+	public void test() {
+		int two = 2;
+		assert(two + two == 4);
+	}
+
+}
