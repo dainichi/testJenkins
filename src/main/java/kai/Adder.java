39c5f39fb428b63f381b7a3443ba8ee95b15dc6b
Kai Dainichi Christensen <dainichi@gmail.com>
2018-12-31 09:23:19 +0900

    added logging

--- src/main/java/kai/Adder.java
+++ src/main/java/kai/Adder.java
@@ -1,10 +1,17 @@
 package kai;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Adder {
+	
+	Logger logger = LoggerFactory.getLogger(Adder.class);
 	
 	public int add(int a, int b)
 	{
-		return a + b;
+		int res = a + b;
+		logger.info("Adding " + a + " and " + b + " getting " + res);
+		return res;
 	}
 
 }
